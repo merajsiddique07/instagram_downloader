5b1f74002d2b9ca8b2dacdf9751b83f389cbf875
Meraj Siddique <133602950+merajsiddique07@users.noreply.github.com>
2025-06-30 17:09:37 +0530

    Update app.py

--- instagram_downloader/app.py
+++ instagram_downloader/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, send_file
 import yt_dlp
 import os
-
+app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     message = ""
